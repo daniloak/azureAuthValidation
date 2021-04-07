b15049f756e71b4e77baf8357b6ff8320d4b57de
Danilo Acceto Kasparian <dacceto@gmail.com>
2021-04-06 23:45:39 -0300

    initial commit

---
+++ src/AuthenticationApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Validate(PostRequest postRequest)
+        {
+            if (postRequest.Email == "danilo@example.com")
+            {
+                return Ok(new { Email = "danilo@example.com" });
+            }
+
+            return Conflict(new { Error = "There is an error" });
+        }
+    }
+}
